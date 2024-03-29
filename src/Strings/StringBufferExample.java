16d2a59f9ae801071c703d6cf632a96d12413201
modithapahan <modithapahan0@gmail.com>
2024-03-30 01:18:10 +0530

    String

---
+++ src/Strings/StringBufferExample.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.append(" World");
+
+        System.out.println(sb.toString());
+    }
+}
