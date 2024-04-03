67bb2c068c721f1bbbf8ec4391da52c599bf7f6a
modithapahan <modithapahan0@gmail.com>
2024-04-03 15:42:16 +0530

    access modifires

---
+++ src/OOP_Concept/Access_Modifiers/Public/p1/A.java
@@ -0,0 +1,7 @@
+package OOP_Concept.Access_Modifiers.Public.p1;
+
+public class A {
+    public void display() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/OOP_Concept/Access_Modifiers/Public/p2/B.java
@@ -0,0 +1,11 @@
+package OOP_Concept.Access_Modifiers.Public.p2;
+
+import OOP_Concept.Access_Modifiers.Public.p1.A;
+
+public class B {
+    public static void main(String[] args) {
+        A obj = new A();
+
+        obj.display();
+    }
+}
