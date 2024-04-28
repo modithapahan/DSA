38173cafc5f7eaeafd3a81fc28da7161ec17fe70
modithapahan <modithapahan0@gmail.com>
2024-04-28 21:56:03 +0530

    array

---
+++ src/Array/Declaration.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Declaration {
+    public static void main(String[] args) {
+        /* Declar an array */
+        int[] arr1;
+        char[] arr2;
+        float[] arr3;
+    }
+}
