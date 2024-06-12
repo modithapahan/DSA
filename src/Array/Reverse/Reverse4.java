33793b705e840ca262596d8776cc7b33f90ea21d
modithapahan <modithapahan0@gmail.com>
2024-06-12 17:48:18 +0530

    array reverse

---
+++ src/Array/Reverse/Reverse4.java
@@ -0,0 +1,32 @@
+package Array.Reverse;
+
+public class Reverse4 {
+    /*
+     * Two Pointer approach
+     */
+
+    public static void reverseArrayTwoPointer(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+        reverseArrayTwoPointer(arr);
+
+        System.out.print("Reversed Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
