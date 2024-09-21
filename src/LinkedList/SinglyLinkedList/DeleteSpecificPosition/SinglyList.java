8c5dd045058ea145c8b3140f8ee88a590fcb1b9d
modithapahan <modithapahan0@gmail.com>
2024-09-21 20:54:32 +0530

    delete node at a specific position

---
+++ src/LinkedList/SinglyLinkedList/DeleteSpecificPosition/SinglyList.java
@@ -0,0 +1,77 @@
+package LinkedList.SinglyLinkedList.DeleteSpecificPosition;
+
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyList {
+    private Node head;
+
+    public int findSize() {
+        Node current = head;
+        int count = 0;
+        while(current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public void deleteNodeAtSpecificPosition(int position) {
+        int listSize = findSize();
+        if(head == null) {
+            System.out.println("Empty List");
+            return;
+        } else if(position == 1) {
+            Node temp1 = head;
+            head = head.next;
+            temp1 = null;
+        } else if (listSize < position || position <= 0) {
+            System.out.println("Position out of bounds");
+            return;
+        } else {
+            Node current = head;
+            int count = 1;
+
+            while(current != null && count < position - 1) {
+                count++;
+                current = current.next;
+            }
+
+            Node temp = current.next;
+            current.next = current.next.next;
+            temp = null;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while(current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        SinglyList sl = new SinglyList();
+
+        sl.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node forth = new Node(4);
+
+        sl.head.next = second;
+        second.next = third;
+        third.next = forth;
+
+        sl.deleteNodeAtSpecificPosition(3);
+        sl.display();
+    }
+}
